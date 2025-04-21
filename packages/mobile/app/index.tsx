42e9171b445faceab303b51d643a48e74fe03ac7
betomoedano <betomoedano@outlook.com>
2025-04-21 16:15:41 -0500

    Remove NewTodo component from the mobile app; update Index component to reflect this change.

--- packages/mobile/app/index.tsx
+++ packages/mobile/app/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { NewTodo } from "../components/NewTodo";
 import { Filters } from "../components/Filters";
 import { ListTodos } from "../components/ListTodos";
 import { ListNotes } from "../components/ListNotes";
@@ -7,7 +6,6 @@ import { ListNotes } from "../components/ListNotes";
 export default function Index() {
   return (
     <>
-      <NewTodo />
       <ListNotes />
       <Filters />
     </>
