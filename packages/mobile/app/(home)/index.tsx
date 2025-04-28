19db840a8cd1f3a60c230d9d3f39fe9ffbfdf8eb
betomoedano <betomoedano@outlook.com>
2025-04-28 15:32:46 -0500

    Add sign out button

--- packages/mobile/app/(home)/index.tsx
+++ packages/mobile/app/(home)/index.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import { use } from "react";
+import { View, Pressable, Text } from "react-native";
 import { ListNotes } from "../../components/ListNotes";
+import { AuthContext } from "../../context/auth";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
-  return <ListNotes />;
+  const { signOut } = use(AuthContext);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ListNotes />
+      <Pressable
+        onPress={signOut}
+        style={{
+          position: "absolute",
+          bottom: 32,
+          left: 16,
+          width: 50,
+          height: 50,
+          backgroundColor: "#000",
+          borderRadius: 25,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Ionicons name="log-out-outline" size={24} color="white" />
+      </Pressable>
+    </View>
+  );
 }
