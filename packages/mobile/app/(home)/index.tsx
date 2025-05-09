cc5f9bdca2099e90e714fc4c5c17e31e8dc37810
betomoedano <betomoedano@outlook.com>
2025-05-09 11:51:43 -0500

    Refactor notes display in mobile app by replacing ListNotes component with FlatList in Index. Enhanced UI to show a message when no notes are available and added styles for better layout.

--- packages/mobile/app/(home)/index.tsx
+++ packages/mobile/app/(home)/index.tsx
@@ -1,15 +1,36 @@
+import React from "react";
+import { FlatList, StyleSheet, Text, View, Pressable } from "react-native";
+import { visibleNotes$ } from "@workshop/shared/queries.ts";
+
 import { use } from "react";
-import { View, Pressable, Text } from "react-native";
-import { ListNotes } from "../../components/ListNotes";
 import { AuthContext } from "../../context/auth";
 import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "@livestore/react";
+import { Note } from "../../components/NoteItem";
 
 export default function Index() {
   const { signOut } = use(AuthContext);
+  const visibleNotes = useQuery(visibleNotes$);
 
   return (
     <View style={{ flex: 1 }}>
-      <ListNotes />
+      <FlatList
+        data={visibleNotes}
+        renderItem={({ item }) => <Note {...item} />}
+        ListEmptyComponent={() => (
+          <View
+            style={{
+              alignItems: "center",
+              marginTop: 24,
+            }}
+          >
+            <Text style={{ color: "gray" }}>No notes yet!</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.container}
+        contentInsetAdjustmentBehavior="automatic"
+      />
       <Pressable
         onPress={signOut}
         style={{
@@ -29,3 +50,10 @@ export default function Index() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: "#F2F2F7",
+  },
+});
